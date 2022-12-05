8065849ffb424219487e1f133783ba64db985f06
thiago lopes de mello <timabuntu@gmail.com>
2022-12-05 14:21:33 -0300

    feat: imported header

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,13 +3,17 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
+import Header from '../Header';
+
 import { Container } from './styles';
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <Container>MyContacts</Container>
+      <Container>
+        <Header />
+      </Container>
     </ThemeProvider>
   );
 }
