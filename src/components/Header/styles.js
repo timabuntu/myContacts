09f658bb351689def762a5e00dcade2df6b28edd
thiago lopes de mello <timabuntu@gmail.com>
2022-12-05 14:35:33 -0300

    feat: stylized logo

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
+  margin-top: 74px;
+
+  img {
+    width: 80px;
+  }
 `;
