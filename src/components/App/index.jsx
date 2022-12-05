6f02699e45d37556af60d688121300f484cca8a8
thiago lopes de mello <timabuntu@gmail.com>
2022-12-05 12:49:14 -0300

    feat: imported container

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,11 +3,13 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
+import { Container } from './styles';
+
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <h1>MyContacts</h1>
+      <Container>MyContacts</Container>
     </ThemeProvider>
   );
 }
