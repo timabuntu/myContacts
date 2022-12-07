9a5ff8a18735dbb8d8aadf1c84cf628076f1ffe4
thiago lopes de mello <timabuntu@gmail.com>
2022-12-07 14:41:31 -0300

    feat: created stylized link

--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -1,5 +1,21 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+margin-top: 32px;
+`;
 
+export const Header = styled.header`
+
+strong{
+  color: #222;
+  font-size: 24px;
+}
+
+a{
+  color: #5061fc;
+  text-decoration: none;
+  border: 2px solid #5061fc;
+  padding: 8px 16px;
+  border-radius: 4px;
+}
 `;
