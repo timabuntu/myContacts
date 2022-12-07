848850e1bc686d68d06293ce3ea9ae45e23f1b23
thiago lopes de mello <timabuntu@gmail.com>
2022-12-07 14:30:51 -0300

    feat: created base contactsList component blank

---
+++ src/components/ContactsList/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+function ContactsList() {
+  return (
+    <Container>
+      <h1>ContactsList</h1>
+    </Container>
+  );
+}
+
+export default ContactsList;
