1b5a2fd5dac8d10a706a1875a5e43689984e9435
thiago lopes de mello <timabuntu@gmail.com>
2022-12-07 14:47:28 -0300

    feat: created primary colors

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -1,3 +1,12 @@
 export default {
-  backgroundColor: '#F6F5FC',
+  colors: {
+    background: '#f6f5fc',
+    primary: {
+      lighter: '#e0e3ff',
+      light: '#6674f4',
+      main: '#5061fc',
+      dark: '#3346f0',
+    },
+
+  },
 };
