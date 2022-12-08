af49ad6cacbdacd7f3e07424f5f1603e6180f155
thiago lopes de mello <timabuntu@gmail.com>
2022-12-08 11:13:39 -0300

    feat: stylized order button

--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -1,32 +1,52 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-margin-top: 32px;
+  margin-top: 32px;
 `;
 
 export const Header = styled.header`
-display: flex;
-text-align: center;
-justify-content: space-between;
-
-strong{
   display: flex;
-  align-items: center;
-  color: #222;
-  font-size: 24px;
-}
+  text-align: center;
+  justify-content: space-between;
 
-a{
-  color: ${({ theme }) => theme.colors.primary.main};
-  text-decoration: none;
-  border: 2px solid ${({ theme }) => theme.colors.primary.main};
-  padding: 8px 16px;
-  border-radius: 4px;
-  transition: all 0.1s ease-in;
-
-  &:hover{
-    background: ${({ theme }) => theme.colors.primary.main};
-    color: ${({ theme }) => theme.colors.primary.lighter}
+  strong {
+    display: flex;
+    align-items: center;
+    color: #222;
+    font-size: 24px;
   }
-}
+
+  a {
+    color: ${({ theme }) => theme.colors.primary.main};
+    text-decoration: none;
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+    padding: 8px 16px;
+    border-radius: 4px;
+    transition: all 0.1s ease-in;
+
+    &:hover {
+      background: ${({ theme }) => theme.colors.primary.main};
+      color: ${({ theme }) => theme.colors.primary.lighter};
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header{
+
+    button{
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      color: ${({ theme }) => theme.colors.primary.main};
+    }
+
+    span{
+      margin-right: 8px;
+      font-weight: bold;
+    }
+  }
 `;
