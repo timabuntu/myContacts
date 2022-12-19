b5aa49ce8c87339c27ee2e6b60f24f57d5a6e926
thiago lopes de mello <timabuntu@gmail.com>
2022-12-19 12:36:41 -0300

    feat: created card component

--- src/components/ContactsList/index.jsx
+++ src/components/ContactsList/index.jsx
@@ -1,13 +1,16 @@
-import { ArrowUp } from 'phosphor-react';
+import {
+  ArrowUp, PencilSimple, TrashSimple,
+} from 'phosphor-react';
 
-import { Container, Header, ListContainer } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
 
 function ContactsList() {
   return (
     <Container>
       <Header>
         <strong>3 contatos</strong>
-        <a href="/">Novo Contato</a>
       </Header>
 
       <ListContainer>
@@ -17,6 +20,70 @@ function ContactsList() {
             <ArrowUp weight="bold" size={20} />
           </button>
         </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Thiago Mello</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>thiagogv2@hotmail.com</span>
+            <span>(15)9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <PencilSimple size={20} />
+            </a>
+            <button type="button">
+              <TrashSimple size={20} />
+            </button>
+          </div>
+
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Thiago Mello</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>thiagogv2@hotmail.com</span>
+            <span>(15)9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <PencilSimple size={20} />
+            </a>
+            <button type="button">
+              <TrashSimple size={20} />
+            </button>
+          </div>
+
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Thiago Mello</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>thiagogv2@hotmail.com</span>
+            <span>(15)9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <PencilSimple size={20} />
+            </a>
+            <button type="button">
+              <TrashSimple size={20} />
+            </button>
+          </div>
+
+        </Card>
 
       </ListContainer>
 
