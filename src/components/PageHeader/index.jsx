b102e41d609474dde8d58cd2588a1ab178235262
thiago lopes de mello <timabuntu@gmail.com>
2022-12-23 09:35:35 -0300

    feat: back button created

--- src/components/PageHeader/index.jsx
+++ src/components/PageHeader/index.jsx
@@ -1,11 +1,24 @@
+import PropTypes from 'prop-types';
+
+import { ArrowUp } from 'phosphor-react';
+import { Link } from 'react-router-dom';
 import { Container } from './styles';
 
-function PageHeader() {
+function PageHeader({ title }) {
   return (
     <Container>
-      <h1>PageHeader</h1>
+      <Link to="/">
+        <ArrowUp weight="bold" size={20} />
+        <span>Voltar</span>
+      </Link>
+
+      <h1>{title}</h1>
     </Container>
   );
 }
+
+PageHeader.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default PageHeader;
