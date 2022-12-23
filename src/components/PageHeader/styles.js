586534805cb94e7d6e68514e5e9948c693c7fbc7
thiago lopes de mello <timabuntu@gmail.com>
2022-12-23 09:34:15 -0300

    style: stylized link and svg

--- src/components/PageHeader/styles.js
+++ src/components/PageHeader/styles.js
@@ -1,5 +1,22 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
 
+    span,
+    svg {
+      color: ${({ theme }) => theme.colors.primary.main};
+      font-weight: bold;
+    }
+    svg {
+      margin-right: 8px;
+      transform: rotate(-90deg);
+    }
+  }
+  h1 {
+    font-size: 24px;
+  }
 `;
