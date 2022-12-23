f5d133d2968a7253ef843dd4345f06dd58cc938c
thiago lopes de mello <timabuntu@gmail.com>
2022-12-23 09:35:49 -0300

    chore: added props

--- src/pages/EditContact/index.jsx
+++ src/pages/EditContact/index.jsx
@@ -1,5 +1,7 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function EditContact() {
   return (
-    <h1>NewContactPage</h1>
+    <PageHeader title="Editar Thiago Mello" />
   );
 }
