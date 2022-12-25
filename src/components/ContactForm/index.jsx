80d940271b717b4e4c598b0b744308d2a92b8d58
thiago lopes de mello <timabuntu@gmail.com>
2022-12-25 10:21:12 -0300

    feat: added inputs e button

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,16 +1,41 @@
-import { Form } from './styles';
+import PropTypes from 'prop-types';
+
+import { ButtonContainer, Form } from './styles';
 
 import FormGroup from '../FormGroup';
 import Input from '../Input';
+import Select from '../Select';
+import Button from '../Button';
 
-function ContactForm() {
+function ContactForm({ buttonLabel }) {
   return (
     <Form>
       <FormGroup>
-        <Input />
+        <Input placeholder="Nome" />
       </FormGroup>
+
+      <FormGroup>
+        <Input placeholder="E-mail" />
+      </FormGroup>
+
+      <FormGroup>
+        <Input placeholder="Telefone" />
+      </FormGroup>
+
+      <FormGroup>
+        <Select>
+          <option value="instagram">InstaGram</option>
+        </Select>
+      </FormGroup>
+
+      <ButtonContainer>
+        <Button type="submit">{buttonLabel}</Button>
+      </ButtonContainer>
     </Form>
   );
 }
 
+ContactForm.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
 export default ContactForm;
