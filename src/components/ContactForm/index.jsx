f567e9e1bde5bfac4f2dade3d7100cd605527c28
thiago lopes de mello <timabuntu@gmail.com>
2023-01-11 11:47:50 -0300

    feat: events form created

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 
+import { useState } from 'react';
 import { ButtonContainer, Form } from './styles';
 
 import FormGroup from '../FormGroup';
@@ -8,28 +9,61 @@ import Select from '../Select';
 import Button from '../Button';
 
 function ContactForm({ buttonLabel }) {
-  return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Nome" />
-      </FormGroup>
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
 
-      <FormGroup
-        error="O formato do e-mail é inválido"
-      >
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({
+      name,
+      email,
+      phone,
+      category,
+    });
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
         <Input
-          placeholder="E-mail"
-          error
+          placeholder="Nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
         />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
-          <option value="instagram">InstaGram</option>
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">
+            Categoria
+          </option>
+          <option value="instagram">
+            Instagram
+          </option>
+          <option value="discord">
+            Discord
+          </option>
         </Select>
       </FormGroup>
 
