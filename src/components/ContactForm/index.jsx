bf135ec409125ded79799989b824403ca51cdaad
thiago lopes de mello <timabuntu@gmail.com>
2023-01-19 16:09:05 -0300

    feat: added mask phone created

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 import isEmailValid from '../../utils/isEmailValid';
+import formatPhone from '../../utils/formatPhone';
 import useErrors from '../../hooks/useErrors';
 
 import { ButtonContainer, Form } from './styles';
@@ -39,14 +40,18 @@ function ContactForm({ buttonLabel }) {
     }
   }
 
+  function handlePhoneChange(event) {
+    setPhone(formatPhone(event.target.value));
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    /* console.log({
+    console.log({
       name,
       email,
-      phone,
+      phone: phone.replace(/\D/g, ''),
       category,
-    }); */
+    });
   }
 
   return (
@@ -74,7 +79,8 @@ function ContactForm({ buttonLabel }) {
         <Input
           placeholder="Telefone"
           value={phone}
-          onChange={(event) => setPhone(event.target.value)}
+          onChange={handlePhoneChange}
+          maxLength="15"
         />
       </FormGroup>
 
